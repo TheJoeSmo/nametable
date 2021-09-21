cccda93cffca846003cd36c92617ded58a12eb4f
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-09-21 15:46:55 -0400

    Refactor Tile to use bytes as the primary attributes.
    The change from an NDArray to bytes is because bytes is hashable.  This makes it much easier to create Patterns.

--- nametable/Tile.py
+++ nametable/Tile.py
@@ -6,42 +6,20 @@ from numpy.typing import NDArray
 
 @dataclass(frozen=True, eq=True)
 class Tile:
-    tile_data: NDArray[ubyte]
+    data: bytes
 
     WIDTH = 8
     HEIGHT = 8
 
-    @classmethod
-    def from_np_bits(cls, lower: NDArray[ubyte], upper: NDArray[ubyte]):
+    @property
+    def numpy_array(self) -> NDArray[ubyte]:
         """
-        Creates a Tile from a NDArray representing a bitmap of the lower and upper bits.
+        An array representation of the Tile.
 
-        Parameters
-        ----------
-        lower : NDArray[ubyte]
-            The bits representing the 0 or 1 of the color index.
-        upper : NDArray[ubyte]
-            The bits representing the 0 or 2 of the color index.
-
-        Notes
-        -----
-        This method is meant primarily for NES styled Tiles.
+        Returns
+        -------
+        NDArray[ubyte]
+            The Tile represented as an array.
         """
-        return cls(add(lower, upper * 2))
-
-    @classmethod
-    def from_bytes(cls, data: bytes):
-        """
-        Creates a Tile from bytes representing a two bits per pixel format.
-
-        Parameters
-        ----------
-        data : bytes
-            The data representing the Tile.
-
-        Notes
-        -----
-        This method is meant primarily for NES styled Tiles.
-        """
-        array = unpackbits(frombuffer(data, dtype=ubyte)).reshape((2, Tile.HEIGHT, Tile.WIDTH))
-        return cls.from_np_bits(array[0, :, :], array[1, :, :])
+        array = unpackbits(frombuffer(self.data, dtype=ubyte)).reshape((2, Tile.HEIGHT, Tile.WIDTH))
+        return add(array[0, :, :], array[1, :, :] * 2)
--- tests/test_tile.py
+++ tests/test_tile.py
@@ -3,9 +3,9 @@ from numpy import array_equal
 from nametable.Tile import Tile
 
 
-def test_initialization(tile_ndarray):
-    Tile(tile_ndarray)
+def test_initialization(tile_bytes):
+    Tile(tile_bytes)
 
 
 def test_bytes_conversion(tile_data):
-    assert array_equal(Tile.from_bytes(tile_data[0]).tile_data, tile_data[1])
+    assert array_equal(Tile(tile_data[0]).numpy_array, tile_data[1])
