3f38c6d7c4df1c523383bd35ffec92b4bb44a091
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-09-26 04:56:18 -0400

    Add property tests

--- nametable/PatternMeta.py
+++ nametable/PatternMeta.py
@@ -17,8 +17,39 @@ class PatternMeta:
     WIDTH = 8
     HEIGHT = 8
 
+    def __eq__(self, other) -> bool:
+        return list(self.data) == list(other.data)
+
+    def __ne__(self, other) -> bool:
+        return list(self.data) != list(other.data)
+
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}({self.data.hex()})"
+
+    @classmethod
+    def from_numpy_array(cls, array: NDArray[ubyte]):
+        """
+        Creates a Pattern from a numpy array of a size (8, 8).
+
+        Parameters
+        ----------
+        array : NDArray[ubyte]
+            The numpy array to be converted.
+        """
+        assert array.shape == (8, 8)
+
+        a = array.flatten()
+        bitmap = [element & 0b1 for element in a] + [(element & 0b10) >> 1 for element in a]
+        ba = bytearray(16)
+        for index, element in enumerate(bitmap):
+            big_endian_offset = 8 * (index // 64)
+            byte_offset = (index // 8) % 8
+            bit_offset = abs(8 - (index % 8)) - 1
+            ba[big_endian_offset + byte_offset] += element << bit_offset
+
+        assert len(ba) == 16
+
+        return cls(bytes(ba))
 
     @property
     def numpy_array(self) -> NDArray[ubyte]:
@@ -30,5 +61,11 @@ class PatternMeta:
         NDArray[ubyte]
             The PatternMeta represented as an array.
         """
+        assert len(self.data) == 16
+
         array = unpackbits(frombuffer(self.data, dtype=ubyte)).reshape((2, self.HEIGHT, self.WIDTH))
-        return add(array[0, :, :], array[1, :, :] * 2)
+        array = add(array[0, :, :], array[1, :, :] * 2)
+
+        assert array.shape == (8, 8)
+
+        return array
--- tests/test_pattern_meta.py
+++ tests/test_pattern_meta.py
@@ -1,10 +1,39 @@
+from hypothesis import given
+from hypothesis.strategies import binary
+
 from numpy import array_equal
 
 from nametable.PatternMeta import PatternMeta
 
 
-def test_initialization(tile_bytes):
-    PatternMeta(tile_bytes)
+@given(binary(min_size=16, max_size=16))
+def test_initialization(bytes):
+    PatternMeta(bytes)
+
+
+@given(binary(min_size=16, max_size=16), binary(min_size=16, max_size=16))
+def test_equality(bytes, bytes_):
+    assert (PatternMeta(bytes) == PatternMeta(bytes_)) == (list(bytes) == list(bytes_))
+
+
+@given(binary(min_size=16, max_size=16), binary(min_size=16, max_size=16))
+def test_inequality(bytes, bytes_):
+    assert (PatternMeta(bytes) != PatternMeta(bytes_)) == (list(bytes) != list(bytes_))
+
+
+@given(binary(min_size=16, max_size=16), binary(min_size=16, max_size=16))
+def test_equality_cannot_be_not_equal(bytes, bytes_):
+    assert (PatternMeta(bytes) == PatternMeta(bytes_)) != (PatternMeta(bytes) != PatternMeta(bytes_))
+
+
+@given(binary(min_size=16, max_size=16))
+def test_conversion(bytes):
+    assert PatternMeta(bytes) == PatternMeta.from_numpy_array(PatternMeta(bytes).numpy_array)
+
+
+@given(binary(min_size=16, max_size=16))
+def test_numpy_array_shape(bytes):
+    assert PatternMeta(bytes).numpy_array.shape == (8, 8)
 
 
 def test_bytes_conversion(tile_data):
