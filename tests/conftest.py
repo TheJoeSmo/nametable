8c45702e25093efe538fd2e13b047b296adfdefd
TheJoeSmo <joesmo.joesmo12@gmail.com>
2021-09-26 22:34:30 -0400

    Remove unused import

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 from typing import Optional
 from pytest import fixture
 from hypothesis.strategies import builds, binary, integers, lists, composite
-from itertools import product
 
 from numpy import array, ubyte
 from numpy.typing import NDArray
